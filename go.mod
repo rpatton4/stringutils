d3238cb047bc85e20f6522bb5b52858571f45d87
Robert Patton <robpatton@infiniteskye.com>
2024-04-07 20:48:39 -0400

    Initial versions with Upper and Lower functions only

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rpatton4/stringutils
+
+go 1.22
---
+++ stringutils.go
@@ -0,0 +1,11 @@
+package stringutils
+
+import "strings"
+
+func Upper(content string) string {
+	return strings.ToUpper(content)
+}
+
+func Lower(content string) string {
+	return strings.ToLower(content)
+}
